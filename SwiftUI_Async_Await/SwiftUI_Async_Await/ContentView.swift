595df6b53ba5d33d23d47ec4e4149cd0db1ca73f
Kant <eileenyoo2@gmail.com>
2023-06-01 23:39:12 +0900

    - SwiftUI + AsyncAwait

---
+++ SwiftUI_Async_Await/SwiftUI_Async_Await/ContentView.swift
@@ -0,0 +1,88 @@
+//
+//  ContentView.swift
+//  SwiftUI_Async_Await
+//
+//  Created by Kant on 2023/06/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var user: GitHubUser?
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.secondary)
+            }
+            .frame(width: 120, height: 120)
+            
+            Text(user?.login ?? "Login Placeholder")
+                .bold()
+                .font(.title3)
+            
+            Text(user?.bio ?? "Bio Placeholder")
+                .padding()
+            
+            Spacer()
+        }
+        .padding()
+        .task {
+            do {
+                user = try await getUser()
+            } catch GHError.invalidURL {
+                print("invalid URL")
+            } catch GHError.invalidResponse {
+                print("invalid Response")
+            } catch GHError.invalidData {
+                print("invalid Data")
+            } catch {
+                print("unexpected error")
+            }
+        }
+    }
+    
+    func getUser() async throws -> GitHubUser {
+        let endpoint = "https://api.github.com/users/sapere4ude"
+        
+        guard let url = URL(string: endpoint) else { throw GHError.invalidURL }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { throw GHError.invalidResponse }
+        
+        do {
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            return try decoder.decode(GitHubUser.self, from: data)
+        } catch {
+            throw GHError.invalidData
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct GitHubUser: Codable {
+    let login: String
+    let avatarUrl: String
+    let bio: String
+}
+
+enum GHError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+}
---
+++ SwiftUI_Async_Await/SwiftUI_Async_Await/SwiftUI_Async_AwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Async_AwaitApp.swift
+//  SwiftUI_Async_Await
+//
+//  Created by Kant on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Async_AwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

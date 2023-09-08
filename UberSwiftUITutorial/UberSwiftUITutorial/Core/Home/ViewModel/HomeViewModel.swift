adf6ca2f970b844610476b12f4ac8af69a4e831a
sapere4ude <eileenyoo2@gmail.com>
2023-09-08 23:50:03 +0900

    - Section10 > 40 Fetch Driver Ride Requests

--- UberSwiftUITutorial/UberSwiftUITutorial/Core/Home/ViewModel/HomeViewModel.swift
+++ UberSwiftUITutorial/UberSwiftUITutorial/Core/Home/ViewModel/HomeViewModel.swift
@@ -60,8 +60,12 @@ class HomeViewModel: NSObject, ObservableObject {
             .sink { user in
                 self.currentUser = user
                 guard let user = user else { return }
-                guard user.accountType == .passenger else { return }
-                self.fetchDrivers()
+                
+                if user.accountType == .passenger {
+                    self.fetchDrivers()
+                } else {
+                    self.fetchTrips()
+                }
             }
             .store(in: &cancellables)
     }
@@ -107,7 +111,16 @@ extension HomeViewModel {
 
 // MARK: - Driver API
 extension HomeViewModel {
-    
+    func fetchTrips() {
+        guard let currentUser = currentUser else { return }
+        
+        Firestore.firestore().collection("trips")
+            .whereField("driverUid", isEqualTo: currentUser.uid)
+            .getDocuments { snapshot, _ in
+                guard let documents = snapshot?.documents, let document = documents.first else { return }
+                guard let trip = try? document.data(as: Trip.self) else { return }
+            }
+    }
 }
 
 // MARK: - Location Search Helpers

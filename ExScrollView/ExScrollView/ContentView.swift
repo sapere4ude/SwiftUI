eed45a67242c9fa175057557dc552c7656038068
sapere4ude <eileenyoo2@gmail.com>
2023-09-30 09:51:12 +0900

    - ExScrollView (애니메이션 추가)

--- ExScrollView/ExScrollView/ContentView.swift
+++ ExScrollView/ExScrollView/ContentView.swift
@@ -17,13 +17,23 @@ struct ContentView: View {
                 ForEach(MockData.items) { item in
                     Circle()
                         .containerRelativeFrame(.horizontal,
-                                                count: verticalSizeClass == .regular ? 5: 4,
+                                                count: verticalSizeClass == .regular ? 1: 4,
                                                 spacing: 16)
                         .foregroundStyle(item.color.gradient)
+                        .scrollTransition { content, phase in
+                            content
+                                .opacity(phase.isIdentity ? 1.0 : 0.5)
+                                .scaleEffect(x: phase.isIdentity ? 1.0 : 0.3,
+                                             y: phase.isIdentity ? 1.0 : 0.3)
+                                .offset(y: phase.isIdentity ? 0 : 50)
+                        }
                 }
             }
+            .scrollTargetLayout()
         }
-        .padding()
+        .contentMargins(16, for: .scrollContent)
+//        .scrollTargetBehavior(.paging) // 페이지에 2개씩 보여줌
+        .scrollTargetBehavior(.viewAligned)
     }
 }
 

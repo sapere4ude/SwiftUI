9cb559a72b18c49aa79c971025ac43aecc99b2fc
sapere4ude <eileenyoo2@gmail.com>
2023-10-14 22:43:23 +0900

    - 항목 추가, 제거 기능 추가

--- ExShoppingBag/ExShoppingBag/ShoppingBag.swift
+++ ExShoppingBag/ExShoppingBag/ShoppingBag.swift
@@ -11,6 +11,9 @@ class ShoppingBag: ObservableObject {
     
     @Published var items: [ShoppingCartItem] = []
 
+    // TODO: - 지금은 함수를 호출하는 형태로 작업을 진행했음
+    // 바꿔야 하는건 선언형으로 변경해야함. bind() 메서드안에 퍼블리셔를 넣는 형식으로다가
+    
     func addItem(_ product: Product) {
         // 이미 장바구니에 있는지 확인
         if let index = items.firstIndex(where: { $0.product == product }) {
@@ -21,9 +24,23 @@ class ShoppingBag: ObservableObject {
             items.append(ShoppingCartItem(product: product, quantity: 1))
         }
     }
+    
+    func removeItem(_ product: Product) {
+        if let index = items.firstIndex(where: { $0.product == product }) {
+            if items[index].quantity > 1 {
+                items[index].quantity -= 1
+            } else {
+                items.remove(at: index)
+            }
+        }
+    }
 
     func removeItem(at index: Int) {
         items.remove(at: index)
+    }
+    
+    func calculateTotalQuantity() -> Int {
+        return items.reduce(0) { $0 + $1.quantity }
     }
 }
 
--- ExShoppingBag/ExShoppingBag/ShoppingCartView.swift
+++ ExShoppingBag/ExShoppingBag/ShoppingCartView.swift
@@ -12,61 +12,103 @@ struct ShoppingCartView: View {
     @ObservedObject var shoppingBag: ShoppingBag
 
     var body: some View {
-        ScrollView {
-            ForEach(shoppingBag.items) { item in
-                HStack {
-                    Image(item.product.imageName)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(height: 100)
-                        .padding(.leading, 5)
-                    VStack {
-                        Text(item.product.name)
-                            .lineLimit(2)
-                            .multilineTextAlignment(.center)
-                        Text("$\(String(format: "%.2f", item.product.price))")
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+        VStack {
+            ScrollView {
+                ForEach(shoppingBag.items) { item in
                     HStack {
+                        Image(item.product.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(height: 100)
+                            .padding(.leading, 5)
+                        VStack {
+                            Text(item.product.name)
+                                .lineLimit(2)
+                                .multilineTextAlignment(.center)
+                            Text("$\(String(format: "%.2f", item.product.price))")
+                        }
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
                         HStack {
-                            Button(action: {
-                                // TODO: - button action
-                            }) {
-                                Image(systemName: "plus.circle.fill")
-                                    .resizable()
-                                    .frame(width: 25, height: 25) // 이미지 크기 조정
-                                    .background(.white) // Circle 모양의 배경
-                                    .foregroundColor(.green)
-                                    .clipShape(Circle())
-                            }
-
-                            // TODO: - 수량 보여주는걸로 바꾸기
-//                            Button("제거") {
-//                                if let index = shoppingBag.items.firstIndex(where: { $0.id == item.id }) {
-//                                    shoppingBag.removeItem(at: index)
-//                                }
-//                            }
-                            Button("\(item.quantity)") {
-                                if let index = shoppingBag.items.firstIndex(where: { $0.id == item.id }) {
-                                    shoppingBag.removeItem(at: index)
+                            HStack {
+                                Button(action: {
+                                    // TODO: - button action
+                                    shoppingBag.addItem(item.product)
+                                }) {
+                                    Image(systemName: "plus.circle.fill")
+                                        .resizable()
+                                        .frame(width: 25, height: 25) // 이미지 크기 조정
+                                        .background(.white) // Circle 모양의 배경
+                                        .foregroundColor(.green)
+                                        .clipShape(Circle())
+                                }
+                                
+                                Text("\(item.quantity)")
+                                
+                                Button(action: {
+                                    // TODO: - button action
+                                    shoppingBag.removeItem(item.product)
+                                }) {
+                                    Image(systemName: "minus.circle.fill")
+                                        .resizable()
+                                        .frame(width: 25, height: 25) // 이미지 크기 조정
+                                        .background(.white) // Circle 모양의 배경
+                                        .foregroundColor(.red)
+                                        .clipShape(Circle())
                                 }
                             }
-                            Button(action: {
-                                // TODO: - button action
-                            }) {
-                                Image(systemName: "minus.circle.fill")
-                                    .resizable()
-                                    .frame(width: 25, height: 25) // 이미지 크기 조정
-                                    .background(.white) // Circle 모양의 배경
-                                    .foregroundColor(.red)
-                                    .clipShape(Circle())
-                            }
                         }
+                        .padding(.trailing, 10)
+                        Spacer()
                     }
-                    .padding(.trailing, 10)
-                    Spacer()
                 }
             }
+            VStack {
+//                HStack(alignment: .top) {
+//                    Text("총 주문 갯수")
+//                    Spacer()
+//                    Button(action: {
+//                        // TODO: -
+//                    }) {
+//                        Text("초기화하기")
+//                    }
+//                    Text("\(shoppingBag.calculateTotalQuantity()) 개")
+//                        .frame(alignment: .trailing)
+//                }
+//                .frame(maxWidth: .infinity, maxHeight: 100)
+//                .background(.pink)
+//                VStack {
+//                    Text("가격")
+//                        .background(.orange)
+//                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                    Text("주문하기")
+//                        .background(.blue)
+//                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                }
+                
+                VStack {
+                    HStack {
+                        Text("총 주문 갯수")
+                        Spacer()
+                        Button(action: {
+                            // TODO: -
+                        }) {
+                            Text("초기화하기")
+                        }
+                        Text("\(shoppingBag.calculateTotalQuantity()) 개")
+                            .frame(alignment: .trailing)
+                    }
+                    VStack {
+                        Text("가격")
+                            .background(.orange)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    }
+                }
+                VStack {
+                    Text("ORDER")
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 100)
         }
     }
 }

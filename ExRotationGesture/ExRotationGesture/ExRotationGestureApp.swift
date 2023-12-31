0a557b1cc9321a80e06ee1a49d13184b3ae17136
sapere4ude <eileenyoo2@gmail.com>
2023-12-31 17:45:31 +0900

    - Rotation Gesture

---
+++ ExRotationGesture/ExRotationGesture/ExRotationGestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExRotationGestureApp.swift
+//  ExRotationGesture
+//
+//  Created by Kant on 12/31/23.
+//
+
+import SwiftUI
+
+@main
+struct ExRotationGestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RotationGestureView()
+        }
+    }
+}
---
+++ ExRotationGesture/ExRotationGesture/RotationGestureView.swift
@@ -0,0 +1,24 @@
+//
+//  RotationGestureView.swift
+//  ExRotationGesture
+//
+//  Created by Kant on 12/31/23.
+//
+
+import SwiftUI
+
+struct RotationGestureView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    RotationGestureView()
+}
